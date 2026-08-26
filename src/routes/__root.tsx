d75eb90cad962e3c4f9e3c3fe8f423abc5f061e4
A.Wolver.P <awolverp@gmail.com>
2026-08-26 15:50:32 +0330

    Initialize

---
+++ src/routes/__root.tsx
@@ -0,0 +1,24 @@
+import { TanStackDevtools } from "@tanstack/react-devtools";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
+
+import "../globals.css";
+
+export const Route = createRootRoute({ component: RootComponent });
+
+function RootComponent() {
+	return (
+		<>
+			<Outlet />
+			<TanStackDevtools
+				config={{ position: "bottom-right" }}
+				plugins={[
+					{
+						name: "TanStack Router",
+						render: <TanStackRouterDevtoolsPanel />,
+					},
+				]}
+			/>
+		</>
+	);
+}
